e5988f71fd177fcf86f5a5abd6e84dc88f298286
AfshalG <afshalkingmaker@gmail.com>
2026-01-18 12:47:25 +0800

    Add Task class for OOP design (A-Classes)

---
+++ src/main/java/Task.java
@@ -0,0 +1,45 @@
+/**
+ * Represents a task with a description and completion status.
+ */
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    /**
+     * Creates a new task with the given description.
+     * Task is initially marked as not done.
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the description of this task.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Returns the status icon of this task.
+     * return "X" if task is done, " " otherwise
+     */
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    /**
+     * Marks this task as done.
+     */
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    /**
+     * Marks this task as not done.
+     */
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+}
