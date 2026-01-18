7a8958bf65f7fde014a11944b2a0ff5a389d3a72
AfshalG <afshalkingmaker@gmail.com>
2026-01-18 12:19:00 +0800

    Add Level-1: Implement echo and exit functionality

--- src/main/java/Genie.java
+++ src/main/java/Genie.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Genie {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
+        // Print greeting
         System.out.println(line);
         System.out.println(" Hello! I'm Genie");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
-    }
+        // Input loop
+        while (true) {
+            System.out.println();
+            String input = scanner.nextLine();
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            } else {
+                // Echo the input
+                System.out.println(" " + input);
+                System.out.println(line);
+            }
+        }
+
+        scanner.close();
+}
 }
